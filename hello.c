b55243f9653d33bc3350663aee3654a103f8e7d5
TaehoLi <43288563+TaehoLi@users.noreply.github.com>
2019-03-20 17:43:39 +0900

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void)
+{
+int i = 0;
+while(i<10){
+i += 1;
+ printf("Hello\n");
+}
+}
---
+++ lab2/thread.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<pthread.h>
+ 
+void* say_hello(void* data)
+{
+    char *str;
+    str = (char*)data;
+    while(1)
+    {
+        printf("%s\n",str);
+        sleep(1);
+    }
+}
+void* say_hello2(void* data)
+{
+    char *str;
+    str = (char*)data;
+    while(1)
+    {
+        printf("%s\n",str);
+        sleep(2);
+    }
+}
+void main()
+{
+    pthread_t t1,t2;
+     
+    pthread_create(&t1,NULL,say_hello,"hello from 1");
+    pthread_create(&t2,NULL,say_hello2,"hello from 2");
+
+    pthread_join(t1,NULL);
+}
+
