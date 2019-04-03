cfd9bfb402e7ae7f366be64eaa2078928eac5f97
TaehoLi <43288563+TaehoLi@users.noreply.github.com>
2019-04-03 11:53:31 +0900

    Update exercise2.c

--- lab3/exercise2.c
+++ lab3/exercise2.c
@@ -1,185 +1,84 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <signal.h>
-#include <pthread.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/stat.h>
 #include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#define MAX_DIGIT 4
-#define MAX_BUTTON 9
+#define MAX_BUFF 32
+#define LINE_BUFF 16
+#define FPGA_TEXT_LCD_DEVICE "/dev/fpga_text_lcd"
 
+// LCD 두 줄에 들어갈 string1,2 선언
+// global variable 이기 때문에 -> thread1,2가 서로 공유할 수 있음
+char name[16];
+char name2[16];
 
-int dev_pid;
-int counter=0;
-int flag=0;
-unsigned char data[4] = {0,0,0,0};
-int dev;
-int dev2;
-
-void usrsignal(int sig)
+void* get_str() // 문자열을 두 번 입력받는 함수
 {
-	if(flag==0){
-		counter = counter;
-		data[0]=data[0];data[1]=data[1];data[2]=data[2];data[3]=data[3];
-		write(dev2,&data,4);
-		read(dev2,&data,4);
-	}
-	else if(flag==1){
-			counter++;
-			if (counter >= 1)
-			{
-				data[3]++;
-				if ((data[3] % 10) == 0)
-				{
-					data[2]++;
-					data[3] = 0;
-					if((data[2]%10)==0)
-					{
-						data[1]++;
-						data[2]=0;
-						if ((data[1] % 10) == 0)
-						{
-							data[0]++;
-							data[1] = 0;
-							if((data[0]%6)==0)
-							{
-								data[0] = 0;
-								counter = 0;
-							}
-						}
-					}
-				}
-				printf("%d %d %d %d\n", data[0], data[1], data[2], data[3]);
-				write(dev2,&data,4);
-				read(dev2,&data,4);
-			}
-	}
-	else if(flag==2){
-			counter = 0;
-			data[0]=0;data[1]=0;data[2]=0;data[3]=0;
-			write(dev2,&data,4);
-			read(dev2,&data,4);
-			flag = 0;
-	}
+	printf("Whats your string? : ");
+	fgets(name, sizeof(name), stdin);
+	name[strlen(name) - 1] = 0;
+	printf("Next string? : ");
+	fgets(name2, sizeof(name), stdin);
+	name2[strlen(name2) - 1] = 0;
 }
-
-void* display()
+//int argc, char **argv
+void* show_lcd() // 받은 문자열을 LCD에 보여주는 함수
 {
-	int fd;
-	char buf[10] = {0};
+	
+	int i;
+	int dev;
+	int str_size;
+	int chk_size;
+	char *ptr;
 
-	(void)signal(SIGUSR1, usrsignal);
-	fd = open("/dev/epit", O_RDWR);
-	dev2 = open("/dev/fpga_fnd", O_RDWR);
+	unsigned char string[32];
+	memset(string,0,sizeof(string));
 
-	dev_pid=getpid();
-	buf[0] = 0xff & dev_pid;
-	buf[1] = 0xff & (dev_pid >> 8);
-	write(fd,buf,sizeof(dev_pid));
-
-	while (1)
+	
+	if(strlen(name)>LINE_BUFF||strlen(name2)>LINE_BUFF)
 	{
-		usleep(10000);
+		printf("16 alphanumeric characters on a line!\n");
 	}
-	close(fd);
-	close(dev2);
-}
-
-void* button()
-{
-	int buff_size;
-	unsigned char push_sw_buff[MAX_BUTTON];
-
-	dev = open("/dev/fpga_push_switch", O_RDWR);
-	buff_size=sizeof(push_sw_buff);
-
-	while(1){
-		usleep(300000);
-		read(dev, &push_sw_buff, buff_size);
-
-		if(push_sw_buff[0] == 1){
-			if (flag == 1){
-				flag = 0;
-			}
-			else if (flag == 0){
-				flag = 1;
-			}
-		}
-		else if(push_sw_buff[1] == 1){
-			if (flag == 1){
-				flag = 0;
-			}
-			else if (flag == 0){
-				flag = 1;
-			}
-		}
-		else if(push_sw_buff[2] == 1){
-			if (flag == 1){
-				flag = 0;
-			}
-			else if (flag == 0){
-				flag = 1;
-			}
-		}
-		else if(push_sw_buff[3] == 1){
-			if (flag == 1){
-				flag = 0;
-			}
-			else if (flag == 0){
-				flag = 1;
-			}
-		}
-		else if(push_sw_buff[4] == 1){
-			if (flag == 1){
-				flag = 0;
-			}
-			else if (flag == 0){
-				flag = 1;
-			}
-		}
-		else if(push_sw_buff[5] == 1){
-			if (flag == 1){
-				flag = 0;
-			}
-			else if (flag == 0){
-				flag = 1;
-			}
-		}
-		else if(push_sw_buff[6] == 1){
-			if (flag == 1){
-				flag = 0;
-			}
-			else if (flag == 0){
-				flag = 1;
-			}
-		}
-		else if(push_sw_buff[7] == 1){
-			if (flag == 1){
-				flag = 0;
-			}
-			else if (flag == 0){
-				flag = 1;
-			}
-		}
-		else if(push_sw_buff[8] == 1){
-			flag = 2;
-		}
+	
+	dev = open(FPGA_TEXT_LCD_DEVICE, O_WRONLY);
+	if (dev<0) {
+		printf("Device open error : %s\n",FPGA_TEXT_LCD_DEVICE);
 	}
+
+	str_size=strlen(name);
+	if(str_size>0) {
+		strncat(string,name,str_size);
+		memset(string+str_size,' ',LINE_BUFF-str_size);
+	}
+	
+	str_size=strlen(name2);
+	if(str_size>0) {
+		strncat(string,name2,str_size);
+		memset(string+LINE_BUFF+str_size,' ',LINE_BUFF-str_size);
+	}
+	
+	write(dev,string,MAX_BUFF);
+	
 	close(dev);
+	
+	printf("LCD top : %s", name);
+	printf("\n");
+	printf("LCD down : %s", name2);
+	printf("\n");
 }
 
-void main()
+void main() // thread1,2 차례로 무한 반복
 {
 	pthread_t t1,t2;
-	while(1){
-		pthread_create(&t2,NULL,display,NULL);
-		pthread_create(&t1,NULL,button,NULL);
+	while(1)
+	{
+		pthread_create(&t1,NULL,get_str,NULL);
 		pthread_join(t1,NULL);
+		pthread_create(&t2,NULL,show_lcd,NULL);
 		pthread_join(t2,NULL);
-	}
+	};
 }
